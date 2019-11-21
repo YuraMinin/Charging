2d812567c15b40630180f9ec643ac453ade6c957
yura <minin.m.yura@mail.ru>
2019-11-21 15:22:27 +0300

    fixed bugs

--- frontend/src/app/account/management-subscription/management-subscription.component.ts
+++ frontend/src/app/account/management-subscription/management-subscription.component.ts
@@ -3,48 +3,43 @@ import {UserService} from '../../model/user.service';
 import {Subscriptions} from '../../model/Subscriptions';
 
 @Component({
-  selector: 'app-management-subscription',
-  templateUrl: './management-subscription.component.html',
-  styleUrls: ['./management-subscription.component.css']
+    selector: 'app-management-subscription',
+    templateUrl: './management-subscription.component.html',
+    styleUrls: ['./management-subscription.component.css']
 })
 export class ManagementSubscriptionComponent implements OnInit {
 
-  constructor(private data: UserService) {
-  }
-
-  @Input() id: number;
-  // @Input() subscription: Subscriptions;
-  private periodOn = 1;
-
-  ngOnInit() {
-    // console.log(this.id);
-  }
-
-  // Change products status
-  changeStatus(): void {
-    let subscription: Subscriptions = new Subscriptions();
-    subscription.id = this.data.userSubscription[this.id].id;
-    subscription.status = this.data.userSubscription[this.id].status;
-    subscription.period = this.data.userSubscription[this.id].period;
-    subscription.name = this.data.userSubscription[this.id].name;
-    subscription.cost = this.data.userSubscription[this.id].cost;
-    subscription = this.data.userSubscription[this.id];
-    if (subscription.status === true) {
-      this.periodOn = 1;
+    constructor(private data: UserService) {
     }
-    subscription.period = this.periodOn;
-    subscription.status = !subscription.status;
-    this.data.userSubscription[this.id] = subscription;
-    this.data.modifyUserSubscription(subscription);
-  }
 
-  setPeriodOn(period: number) {
-    this.periodOn = period;
-  }
+    // @Input() id: number;
+    @Input() subscription: Subscriptions;
+    private periodOn = 1;
 
-  getPeriodOn(): number {
-    return this.periodOn;
-  }
+    ngOnInit() {
+        // console.log(this.id);
+    }
+
+    // Change products status
+    changeStatus(): void {
+        let subscription: Subscriptions;
+        subscription = this.subscription;
+        if (subscription.status === true) {
+            this.periodOn = 1;
+        }
+        subscription.period = this.periodOn;
+        subscription.status = !subscription.status;
+        // this.data.userSubscription[this.id] = subscription;
+        this.data.modifyUserSubscription(subscription);
+    }
+
+    setPeriodOn(period: number) {
+        this.periodOn = period;
+    }
+
+    getPeriodOn(): number {
+        return this.periodOn;
+    }
 
 
 }
